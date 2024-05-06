f7217d70935cc1cacf00c2022a847ca073f80fd8
Yosr Ben Cheikh Brehim <yosrbencheikh28@gmail.com>
2024-05-06 13:42:28 +0100

    chapitre 13

--- src/Controller/mixedcontroller.php
+++ src/Controller/mixedcontroller.php
@@ -35,9 +35,8 @@ class mixedcontroller extends AbstractController
     }
 
     #[Route('/mix/{id}', name: 'app_mix_show')]
-    public function show($id, VinylMixRepository $mixRepository): Response
+    public function show(VinylMix $mix): Response
     {
-        $mix = $mixRepository->find($id);
 
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
